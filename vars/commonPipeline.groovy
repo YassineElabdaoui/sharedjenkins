bceb5f9c57a854b3069a05cff9481531bf5fd8e0
YassineElabdaoui <103153421+YassineElabdaoui@users.noreply.github.com>
2024-05-20 11:33:42 +0100

    Update commonPipeline.groovy

--- vars/commonPipeline.groovy
+++ vars/commonPipeline.groovy
@@ -1,1 +1,43 @@
+def call(Map params) {
+    pipeline {
+        agent any
+        tools {
+            maven "maven_3_9_5"
+        }
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    git url: params.repoUrl, branch: params.branch ?: 'main'
+                }
+            }
+            stage('Build Maven') {
+                steps {
+                    dir(params.serviceDir) {
+                        bat 'mvn clean install'
+                    }
+                }
+            }
+            stage('Build Docker image') {
+                steps {
+                    script {
+                        dir(params.serviceDir) {
+                            bat "docker build -t ${params.dockerImage} ."
+                        }
+                    }
+                }
+            }
+            stage('Push image to hub') {
+                steps {
+                    script {
+                        withCredentials([string(credentialsId: params.dockerCredentialsId, variable: 'dockerhubpwd')]) {
+                            bat "docker login -u ${params.dockerUsername} -p ${dockerhubpwd}"
+                        }
+                        bat "docker push ${params.dockerImage}:latest"
+                    }
+                }
+            }
+        }
+    }
+}
 
